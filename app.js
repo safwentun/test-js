0c54fdbf037d4871e80c650bda94651c4b620b8c
“safwentun” <safwen.saidi@gmail.com>
2023-01-15 15:35:53 +0100

    second change

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello there")
+//console.log("hello there")
+//console.log(1+1)
+//let Name = prompt("add your name")
+//let Lastname = prompt("add your last name")
+//console.log(" my name  is "  + Name + " my lastname  is " + Lastname)
+//-----------------
+let N1 = prompt ("add n°1")
+let N2 = prompt ("add n°2")
+alert (parseInt(N1)+parseInt(2))
+
